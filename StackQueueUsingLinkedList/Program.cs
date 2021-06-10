0d1decd069f9adf472c492c8925197b54036653b
sohail-ahamed-qureshi <sohailqureshi82@gmail.com>
2021-06-10 23:01:51 +0530

    [sohail] Add: Welcome message

---
+++ StackQueueUsingLinkedList/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StackQueueUsingLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stack and Queue Program statements");
+        }
+    }
+}
